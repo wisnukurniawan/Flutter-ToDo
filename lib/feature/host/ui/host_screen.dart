99da2a0b9a315bb3ff75c2d80b6c21b1fce1f735
wisnu kurniawan <wisnuko13@gmail.com>
2023-03-21 17:56:32 +0700

    Fix build

--- lib/feature/host/ui/host_screen.dart
+++ lib/feature/host/ui/host_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_todo_list/feature/host/bloc/host_bloc.dart';
 import 'package:flutter_todo_list/feature/host/bloc/host_event.dart';
 import 'package:flutter_todo_list/feature/host/bloc/host_state.dart';
-import 'package:flutter_todo_list/l10n/l10n.dart';
 import 'package:flutter_todo_list/runtime/navigation/main_router.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
